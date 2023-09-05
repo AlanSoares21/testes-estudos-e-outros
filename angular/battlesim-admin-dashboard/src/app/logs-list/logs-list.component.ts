5558779bc13d58eba91c0018ac8b85e43d3fb03e
AlanSoares21 <alansoares26@outlook.com>
2023-09-05 17:30:03 -0300

    using table component in logs list

--- angular/battlesim-admin-dashboard/src/app/logs-list/logs-list.component.ts
+++ angular/battlesim-admin-dashboard/src/app/logs-list/logs-list.component.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { LogData } from '../log-data';
 import { ApiCallsService } from '../api-calls.service';
+import { TableComponent } from '../table/table.component';
+import { TableHeader } from '../table-header';
 
 @Component({
   selector: 'app-logs-list',
   templateUrl: './logs-list.component.html',
-  styleUrls: ['./logs-list.component.css']
+  styleUrls: ['./logs-list.component.css'],
+  providers: [TableComponent]
 })
 export class LogsListComponent {
   logs: LogData[] = []
+  headers: TableHeader[] = [
+    {id: 'type', fieldName: 'type', displayText: 'Type', width: '10%'},
+    {id: 'message', fieldName: 'message', displayText: 'Message'},
+    {id: 'hour', width: '10%', displayText: 'Hour'},
+    {id: 'check', width: '5%', displayText: 'Check', 
+      input: {
+        type: 'checkbox',
+        checked: (data: LogData) => data.checked,
+        disabled: (data: LogData) => data.checked
+      }
+    }
+  ]
 
   constructor(private api: ApiCallsService) {}
 
