0e1431e5f3f19a6b5a6a2b6a409c125b9bad700e
AlanSoares21 <alansoares26@outlook.com>
2023-09-05 17:13:06 -0300

    using the table component in players list screen

--- angular/battlesim-admin-dashboard/src/app/players-list/players-list.component.ts
+++ angular/battlesim-admin-dashboard/src/app/players-list/players-list.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Player } from '../player';
 import { ApiCallsService } from '../api-calls.service';
 import { Observable, finalize, forkJoin } from 'rxjs';
+import { TableComponent } from '../table/table.component';
+import { TableHeader } from '../table-header';
 
 interface PlayerData extends Player {
   toBlacklist: boolean
@@ -14,19 +16,31 @@ function formatPlayer(player: Player): PlayerData {
 @Component({
   selector: 'app-players-list',
   templateUrl: './players-list.component.html',
-  styleUrls: ['./players-list.component.css']
+  styleUrls: ['./players-list.component.css'],
+  providers: [TableComponent]
 })
 export class PlayersListComponent {
   players: PlayerData[] = []
   sendingPlayersToBlacklist: boolean = false
 
+  tableHeaders: TableHeader[] = [
+    {id: 'name', displayText: 'Name', fieldName: 'entityName'},
+    {id: 'email', displayText: 'Email', fieldName: 'email', width: '20%'},
+    {id: 'lastLogin', displayText: 'Last Login', fieldName: 'lastLogin', width: '20%'},
+    {id: 'ip', displayText: 'Ip', fieldName: 'ip', width: '20%'},
+    {id: 'select', displayText: 'Select', width: '10%', input: {
+      type: 'checkbox',
+      change: (data, index) => {
+        this.check(index)
+      },
+      disabled: (player: PlayerData) => player.blacklist,
+      checked: (player: PlayerData) => player.toBlacklist
+    }}
+  ]
+
   constructor(private api: ApiCallsService) {}
 
-  check(id: string) {
-    const index = this.players.findIndex(p => p.id === id)
-    if (index === -1)
-      return console.error(`Player with id ${id} not found`)
-    
+  check(index: number) {  
     this.players[index].toBlacklist = !this.players[index].toBlacklist
   }
 
